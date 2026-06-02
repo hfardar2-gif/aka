31588cd4f62d06f8e1b794467b3a026d3ca6158b
hfardar2-gif <h.fardar2@gmail.com>
2026-06-02 23:18:04 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -18,9 +18,7 @@ console.log(
 
 
 
-        renderExecutiveSummary(
-            data.totalProductionTill
-        );
+       
 
         renderMaterialFlow(
             data.totalProductionTill
@@ -45,96 +43,6 @@ console.log(
 
 }
 
-/* EXECUTIVE SUMMARY */
-
-function renderExecutiveSummary(data){
-
-    const summaryGrid =
-    document.getElementById('summaryGrid');
-
-    summaryGrid.innerHTML = `
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                FACTORY INPUT
-            </div>
-
-            <div class="summary-value">
-                ${data.inputCoilsTon}
-                <span>T</span>
-            </div>
-
-        </div>
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                PICKLED
-            </div>
-
-            <div class="summary-value">
-                ${data.picklingTon}
-                <span>T</span>
-            </div>
-
-        </div>
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                ROLLED
-            </div>
-
-            <div class="summary-value">
-                ${data.rollingTon}
-                <span>T</span>
-            </div>
-
-        </div>
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                GALVANIZED
-            </div>
-
-            <div class="summary-value">
-                ${data.galvanizedTon}
-                <span>T</span>
-            </div>
-
-        </div>
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                SOLD
-            </div>
-
-            <div class="summary-value">
-                ${data.soldTon}
-                <span>T</span>
-            </div>
-
-        </div>
-
-        <div class="summary-card">
-
-            <div class="summary-label">
-                INPUT COILS
-            </div>
-
-            <div class="summary-value">
-                ${data.inputCoilsQty}
-                <span>QTY</span>
-            </div>
-
-        </div>
-
-    `;
-
-}
 
 /* MATERIAL FLOW */
 
