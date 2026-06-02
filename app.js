6a889cb4038e8f3928435d7fe8c41defcadfe072
hfardar2-gif <h.fardar2@gmail.com>
2026-06-02 22:23:20 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,109 @@
 
-async function loadReport(){
+async function loadDashboard(){
 
-    const response =
-    await fetch('./data/latest-report.json');
+    try{
 
-    const data =
-    await response.json();
+        const response =
+        await fetch('./data/latest-report.json');
 
-    console.log(data);
+        const data =
+        await response.json();
+
+        console.log(data);
+
+        renderExecutiveSummary(
+            data.totalProductionTill
+        );
+
+    }
+
+    catch(error){
+
+        console.error(
+            'Dashboard Load Error:',
+            error
+        );
+
+    }
 
 }
 
-loadReport();
+function renderExecutiveSummary(data){
+
+    const summaryGrid =
+    document.getElementById('summaryGrid');
+
+    summaryGrid.innerHTML = `
+
+        <div class="summary-card">
+            <div class="summary-label">
+                FACTORY INPUT
+            </div>
+
+            <div class="summary-value">
+                ${data.inputCoilsTon}
+                <span>T</span>
+            </div>
+        </div>
+
+        <div class="summary-card">
+            <div class="summary-label">
+                PICKLED
+            </div>
+
+            <div class="summary-value">
+                ${data.picklingTon}
+                <span>T</span>
+            </div>
+        </div>
+
+        <div class="summary-card">
+            <div class="summary-label">
+                ROLLED
+            </div>
+
+            <div class="summary-value">
+                ${data.rollingTon}
+                <span>T</span>
+            </div>
+        </div>
+
+        <div class="summary-card">
+            <div class="summary-label">
+                GALVANIZED
+            </div>
+
+            <div class="summary-value">
+                ${data.galvanizedTon}
+                <span>T</span>
+            </div>
+        </div>
+
+        <div class="summary-card">
+            <div class="summary-label">
+                SOLD
+            </div>
+
+            <div class="summary-value">
+                ${data.soldTon}
+                <span>T</span>
+            </div>
+        </div>
+
+        <div class="summary-card">
+            <div class="summary-label">
+                INPUT COILS
+            </div>
+
+            <div class="summary-value">
+                ${data.inputCoilsQty}
+                <span>QTY</span>
+            </div>
+        </div>
+
+    `;
+
+}
+
+loadDashboard();
 
