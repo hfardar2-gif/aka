9ca12a20fe6b3c29a71711bcb36779c1cb73a4ec
hfardar2-gif <h.fardar2@gmail.com>
2026-06-02 22:44:54 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ async function loadDashboard(){
             data.totalProductionTill
         );
 
+        renderMaterialFlow(
+            data.totalProductionTill
+        );
+
     }
 
     catch(error){
@@ -28,7 +32,98 @@ async function loadDashboard(){
 
 }
 
+/* EXECUTIVE SUMMARY */
+
 function renderExecutiveSummary(data){
+
+    const summaryGrid =
+    document.getElementById('summaryGrid');
+
+    summaryGrid.innerHTML = `
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                FACTORY INPUT
+            </div>
+
+            <div class="summary-value">
+                ${data.inputCoilsTon}
+                <span>T</span>
+            </div>
+
+        </div>
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                PICKLED
+            </div>
+
+            <div class="summary-value">
+                ${data.picklingTon}
+                <span>T</span>
+            </div>
+
+        </div>
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                ROLLED
+            </div>
+
+            <div class="summary-value">
+                ${data.rollingTon}
+                <span>T</span>
+            </div>
+
+        </div>
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                GALVANIZED
+            </div>
+
+            <div class="summary-value">
+                ${data.galvanizedTon}
+                <span>T</span>
+            </div>
+
+        </div>
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                SOLD
+            </div>
+
+            <div class="summary-value">
+                ${data.soldTon}
+                <span>T</span>
+            </div>
+
+        </div>
+
+        <div class="summary-card">
+
+            <div class="summary-label">
+                INPUT COILS
+            </div>
+
+            <div class="summary-value">
+                ${data.inputCoilsQty}
+                <span>QTY</span>
+            </div>
+
+        </div>
+
+    `;
+
+}
+
+/* MATERIAL FLOW */
 
 function renderMaterialFlow(data){
 
@@ -103,86 +198,6 @@ function renderMaterialFlow(data){
 
 }
 
-    const summaryGrid =
-    document.getElementById('summaryGrid');
-
-    summaryGrid.innerHTML = `
-
-        <div class="summary-card">
-            <div class="summary-label">
-                FACTORY INPUT
-            </div>
-
-            <div class="summary-value">
-                ${data.inputCoilsTon}
-                <span>T</span>
-            </div>
-        </div>
-
-        <div class="summary-card">
-            <div class="summary-label">
-                PICKLED
-            </div>
-
-            <div class="summary-value">
-                ${data.picklingTon}
-                <span>T</span>
-            </div>
-        </div>
-
-        <div class="summary-card">
-            <div class="summary-label">
-                ROLLED
-            </div>
-
-            <div class="summary-value">
-                ${data.rollingTon}
-                <span>T</span>
-            </div>
-        </div>
-
-        <div class="summary-card">
-            <div class="summary-label">
-                GALVANIZED
-            </div>
-
-            <div class="summary-value">
-                ${data.galvanizedTon}
-                <span>T</span>
-            </div>
-        </div>
-
-        <div class="summary-card">
-            <div class="summary-label">
-                SOLD
-            </div>
-
-            <div class="summary-value">
-                ${data.soldTon}
-                <span>T</span>
-            </div>
-        </div>
-
-        <div class="summary-card">
-            <div class="summary-label">
-                INPUT COILS
-            </div>
-
-            <div class="summary-value">
-                ${data.inputCoilsQty}
-                <span>QTY</span>
-            </div>
-        </div>
-
-    `;
-
-}
+/* START */
 
 loadDashboard();
-
-renderMaterialFlow(
-    data.totalProductionTill
-);
-
-
-
