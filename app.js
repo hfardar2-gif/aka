2c2838f1721e8a73a5272ed6acaff4fd4f078088
hfardar2-gif <h.fardar2@gmail.com>
2026-06-04 20:48:03 +0330

    Add files via upload

--- app.js
+++ app.js
@@ -38,98 +38,100 @@ function renderMaterialFlow(data){
 
     flowGrid.innerHTML = `
 
-    <div class="plant-card">
+        <div class="plant-card">
 
-        <div class="plant-icon">🧪</div>
+            <div class="plant-title">
+                PICKLING
+            </div>
 
-        <div class="plant-title">
-            PICKLING LINE
+            <div class="plant-row">
+                <span>Production</span>
+                <strong>
+                    ${data.picklingTon.toFixed(1)} T
+                </strong>
+            </div>
+
+            <div class="plant-row">
+                <span>Warehouse</span>
+                <strong>
+                    153.3 T
+                </strong>
+            </div>
+
+            <div class="plant-row">
+                <span>Yield</span>
+                <strong class="good">
+                    100%
+                </strong>
+            </div>
+
         </div>
 
-        <div class="plant-row">
-            <span>Production</span>
-            <strong>
-                ${data.picklingTon.toFixed(1)} T
-            </strong>
+        <div class="process-arrow">
+            →
         </div>
 
-        <div class="plant-row">
-            <span>Warehouse</span>
-            <strong>
-                153.3 T
-            </strong>
+        <div class="plant-card">
+
+            <div class="plant-title">
+                COLD ROLLING
+            </div>
+
+            <div class="plant-row">
+                <span>Production</span>
+                <strong>
+                    ${data.rollingTon.toFixed(1)} T
+                </strong>
+            </div>
+
+            <div class="plant-row">
+                <span>Warehouse</span>
+                <strong>
+                    837.2 T
+                </strong>
+            </div>
+
+            <div class="plant-row">
+                <span>Yield</span>
+                <strong class="warning">
+                    53.7%
+                </strong>
+            </div>
+
         </div>
 
-        <div class="plant-row">
-            <span>Yield</span>
-            <strong class="good">
-                100%
-            </strong>
+        <div class="process-arrow">
+            →
         </div>
 
-    </div>
+        <div class="plant-card">
 
-    <div class="plant-card">
+            <div class="plant-title">
+                GALVANIZING
+            </div>
 
-        <div class="plant-icon">⚙️</div>
+            <div class="plant-row">
+                <span>Production</span>
+                <strong>
+                    ${data.galvanizedTon.toFixed(1)} T
+                </strong>
+            </div>
 
-        <div class="plant-title">
-            COLD ROLLING
+            <div class="plant-row">
+                <span>Ready To Ship</span>
+                <strong>
+                    757.9 T
+                </strong>
+            </div>
+
+            <div class="plant-row">
+                <span>Yield</span>
+                <strong class="good">
+                    97.5%
+                </strong>
+            </div>
+
         </div>
-
-        <div class="plant-row">
-            <span>Production</span>
-            <strong>
-                ${data.rollingTon.toFixed(1)} T
-            </strong>
-        </div>
-
-        <div class="plant-row">
-            <span>Warehouse</span>
-            <strong>
-                837.2 T
-            </strong>
-        </div>
-
-        <div class="plant-row">
-            <span>Yield</span>
-            <strong class="warning">
-                53.7%
-            </strong>
-        </div>
-
-    </div>
-
-    <div class="plant-card">
-
-        <div class="plant-icon">🏭</div>
-
-        <div class="plant-title">
-            GALVANIZING
-        </div>
-
-        <div class="plant-row">
-            <span>Production</span>
-            <strong>
-                ${data.galvanizedTon.toFixed(1)} T
-            </strong>
-        </div>
-
-        <div class="plant-row">
-            <span>Ready To Ship</span>
-            <strong>
-                757.9 T
-            </strong>
-        </div>
-
-        <div class="plant-row">
-            <span>Yield</span>
-            <strong class="good">
-                97.5%
-            </strong>
-        </div>
-
-    </div>
 
     `;
 
